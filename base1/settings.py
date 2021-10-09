f276e1588c68e30e58bb9ffc28f8a8bc29f6f6bc
unknown <msaadanwaqar@gmail.com>
2021-10-08 19:04:54 -0700

    Dtabase Commit

--- base1/settings.py
+++ base1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2l-m!fvi$(ej1iyw)0xvdzbi*3&m_h1t_1tub)0d$bcq%$%sb6'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
